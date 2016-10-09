f97af621e2a999414ecbb20a93a9e353ee095ca9
Weston Nesseler <nesselerweston@yahoo.com>
2016-10-08 19:19:01 -0500

    Added rate limiter

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.use(function(err, req, res, next) {
 
 var request = require('request');
 var sentiment = require('sentiment');
+var RateLimiter = require('limiter').RateLimiter;
+var limiter = new RateLimiter(1, 1000);
 var sentimentResponses = [];
 var forbidden;
 var numberOfTimeouts = 0;
@@ -68,9 +70,14 @@ var articleName = [];
 var datePublished = [];
 var score = [];
 
+var q = "trump";
+var begin_date = 2000;
+var end_date = 2016;
+
+callthis(q, begin_date, end_date);
+
 function callthis(q, begin_date, end_date) {
 	articles = 0;
-	// TODO: initialize arrays to null if james doesn't want to use articles as an index
 	var differenceInDates = end_date - begin_date;
 	begin_date *= 10000;
 	begin_date += 101;
@@ -87,91 +94,97 @@ function callthis(q, begin_date, end_date) {
 		getScores(q, begin_date + 2*differenceInDates, end_date);
 		
 	} else {
+		
 		getScores(q, begin_date, end_date);
 	}
 }
 
 function getScores(q, begin_date, end_date) {
-	var q = "war";
-	var begin_date = "19300101";
-	var end_date = "20100101";
 	do {
-		console.log("Attempting to get body\n");
-		request.get({
-			url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-			qs: {
-				'api-key': "bc500cd222c46649f60f333c6523236",
-				'q': q, // "war",
-				'begin_date': begin_date, // "19300101",
-				'end_date': end_date, // "19400101",
-				'fl': "lead_paragraph,abstract,headline,pub_date"
-			},
-		}, function(err, response, body) {
-			if (body.charAt(0) == '<') {
-				forbidden = true;
-				numberOfTimeouts++;
-				return true;	
-			} else {
-				forbidden = false;
-			}
-			
-			body = JSON.parse(body);
-			
-			if (body.response.meta.hits == 0) {
-				forbidden = true;
-				numberOfTimeouts++;
-				return true;
-			}
-			
-			console.log(body);
-
-			var result;
-			var totalScore;
-			var totalWords;
-			
-			for (var i = 0; i < 10; i++) {
-				totalScore = 0;
-				totalWords = 0;
-				console.log("\n\t\tArticle %d\n", i + 1)
-				if (body.response.docs[i].headline.main) {  
-					console.log("headline:%s\n", body.response.docs[i].headline.main);
-					result = sentiment(body.response.docs[i].headline.main);
-					totalScore += result.score;
-					totalWords += WordCount(body.response.docs[i].headline.main);
-					console.log("headline score:%d\n", result.score);
-				}
-			  
-				if (body.response.docs[i].lead_paragraph) {
-					console.log("lead_paragraph:%s\n", body.response.docs[i].lead_paragraph);
-					result = sentiment(body.response.docs[i].lead_paragraph);
-					totalScore += result.score;
-					totalWords += WordCount(body.response.docs[i].lead_paragraph);
-					console.log("lead_paragraph score:%d\n", result.score);
-				}
-			  
-				if (body.response.docs[i].abstract) {
-					console.log("abstract:%s\n", body.response.docs[i].abstract);
-					result = sentiment(body.response.docs[i].abstract);
-					totalScore += result.score;
-					totalWords += WordCount(body.response.docs[i].abstract);
-					console.log("abstract score:%d\n", result.score);
+		limiter.removeTokens(1, function() {
+			console.log("Attempting to get body\n");
+			request.get({
+				url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+				qs: {
+					'api-key': "bc500cd222c46649f60f333c6523236",
+					'q': q,
+					'fq': "type_of_material:(\"Addendum\", \"An Analysis\", \"Article\", \"Biography\", \"Brief\", \"Chronology\", \"Column\", \"Economic Analysis\", \"Editorial\", \"First Chapter\", \"Interview\", \"Military Analysis\", \"News\", \"News Analysis\", \"Special Report\", \"Summary\", \"Text\")",
+					'begin_date': begin_date,
+					'end_date': end_date,
+					'fl': "lead_paragraph,abstract,headline,pub_date,type_of_material"
+				},
+			}, function(err, response, body) {
+								
+				if (body.charAt(0) == '<') {
+					forbidden = true;
+					numberOfTimeouts++;
+					return true;	
+				} else {
+					forbidden = false;
 				}
 				
-				totalWords += 10;
-				totalScore /= totalWords;
-				console.log("total score:%d", totalScore);
-				if (totalScore < -.1 || totalScore > .1) {
-					articleName[articles] = body.response.docs[i].headline.main;
-					datePublished[articles] = body.response.docs[i].pub_date;
-					score[articles] = totalScore;
-					articles++;
+				body = JSON.parse(body);
+				
+				console.log(body);
+				
+				/*
+				if (body.response.meta.hits == 0) {
+					forbidden = true;
+					numberOfTimeouts++;
+					return true;
 				}
-			}
-			
-			for (i = 0; i < articles; i++) {
-				console.log("name:%s\t\tdate published:%s\t\tscore:%d\n", articleName[i], datePublished[i], score[i]);
-			}
-		})
+				*/
+
+				var result;
+				var totalScore;
+				var totalWords;
+				
+				for (var i = 0; i < 10; i++) {
+					totalScore = 0;
+					totalWords = 0;
+					console.log("\n\t\tArticle %d\n", i + 1)
+					if (body.response.docs[i].headline.main) {  
+						console.log("headline:%s\n", body.response.docs[i].headline.main);
+						result = sentiment(body.response.docs[i].headline.main);
+						totalScore += result.score;
+						totalWords += WordCount(body.response.docs[i].headline.main);
+						console.log("headline score:%d\n", result.score);
+					}
+				  
+					if (body.response.docs[i].lead_paragraph) {
+						console.log("lead_paragraph:%s\n", body.response.docs[i].lead_paragraph);
+						result = sentiment(body.response.docs[i].lead_paragraph);
+						totalScore += result.score;
+						totalWords += WordCount(body.response.docs[i].lead_paragraph);
+						console.log("lead_paragraph score:%d\n", result.score);
+					}
+				  
+					if (body.response.docs[i].abstract) {
+						console.log("abstract:%s\n", body.response.docs[i].abstract);
+						result = sentiment(body.response.docs[i].abstract);
+						totalScore += result.score;
+						totalWords += WordCount(body.response.docs[i].abstract);
+						console.log("abstract score:%d\n", result.score);
+					}
+					
+					totalWords += 10;
+					totalScore /= totalWords;
+					console.log("total score:%d", totalScore);
+					if (totalScore < -.07 || totalScore > .07) {
+						articleName[articles] = body.response.docs[i].headline.main;
+						datePublished[articles] = body.response.docs[i].pub_date;
+						// only first 10 characters of datePublished necessary
+						datePublished[articles] = datePublished[articles].substring(0,10);
+						score[articles] = totalScore;
+						articles++;
+					}
+				}
+				
+				for (i = 0; i < articles; i++) {
+					console.log("name:%s\t\tdate published:%s\t\tscore:%d\n", articleName[i], datePublished[i], score[i]);
+				}
+			})
+		});
 	} while (forbidden && numberOfTimeouts < 5);
 }
 
